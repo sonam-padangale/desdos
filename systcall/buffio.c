48120b5142644245b300164192cbd780564786b2
sonam-padangale <sdpadangale@gmail.com>
2020-10-22 12:25:53 +0530

    Buffio demo

---
+++ systcall/buffio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc , char const *argv[])
+{
+    // printf("Hello World \n");
+    // printf("Hello");
+     printf("Hello \n");
+    printf("World \n");
+    return 0;
+}
